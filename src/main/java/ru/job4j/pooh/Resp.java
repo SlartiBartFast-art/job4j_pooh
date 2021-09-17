124867afffab0bab707af977d9b1947ccc4c1a36
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-17 20:15:43 +0500

    2. Тестовое задание - проект "Pooh JMS" [#268841] Resp class #001

---
+++ src/main/java/ru/job4j/pooh/Resp.java
@@ -0,0 +1,29 @@
+package ru.job4j.pooh;
+
+/**
+ * 2. Тестовое задание - проект "Pooh JMS" [#268841]
+ * Уровень : 3. Мидл Категория : 3.1. Multithreading
+ * Топик : 3.1.7. Контрольные вопросы
+ * часть 3
+ * Resp - ответ от сервиса.
+ * @author SlartiBartFast-art
+ * @version 01
+ * @since 17.09.2021
+ */
+public class Resp {
+    private final String text;
+    private final int status;
+
+    public Resp(String text, int status) {
+        this.text = text;
+        this.status = status;
+    }
+
+    public String text() {
+        return text;
+    }
+
+    public int status() {
+        return status;
+    }
+}
